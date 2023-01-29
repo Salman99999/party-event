2d2d97c5390140636716778373b96a74578c1629
Salman99999 <120678025+Salman99999@users.noreply.github.com>
2023-01-29 17:27:56 +0400

    First Update
    First Update

---
+++ main.js
@@ -0,0 +1,8 @@
+let car = prompt("What is your favourite car?");
+
+if(car == "mercedes"){
+    alert("You are lucky! You can win Mercedes G63 in this party!");
+}
+else{
+    alert("That is good car but in this party you can win Mercedes G63!");
+}
